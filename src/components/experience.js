8fe7bafc08276ed185036f7f49cdb84bd23fec5f
dsadfj ryop <purpenokid@mail.fresnostate.edu>
2019-10-02 15:36:56 -0700

    haha

--- src/components/experience.js
+++ src/components/experience.js
@@ -9,7 +9,7 @@ class Experience extends Component{
             </Cell>
             <Cell col= {8}>
             <h4 style= {{marginTop:'0px'}}>{this.props.jobName}</h4>
-            <p>{this.props.jobDescription}</p>
+            <p>{this.props.jobDescription}s</p>
 
             </Cell>
                 </Grid>
